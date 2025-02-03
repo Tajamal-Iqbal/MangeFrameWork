e9f6f6eff9bb4079e5d6274367d8211e0cffc173
Tajamal Iqbal <tajamal.logiqon@gmail.com>
2025-02-03 11:42:02 +0500

    FrameWork Code

---
+++ ManageLibrary/Mange-Mii/Tester.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tajamal on 03/02/2025.
+//
+
+import Foundation
+import UIKit
+
+
+class SplashViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor =  #colorLiteral(red: 0.9882352941, green: 0.9882352941, blue: 1, alpha: 1) //AppColors.tabBarBackGroud
+        // Perform action based on the URL
+        print("Library confrigureds")
+    }
+}
